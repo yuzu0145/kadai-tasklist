da303af02f52c4c80bd16bc8ffca72698b331514
yuuki.horie <yuzu0145.h@gmail.com>
2020-06-02 00:12:42 +0000

    add bootstrap design

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Task.create(status: 'test title 1', content: 'test content 1')
+Task.create(status: 'test title 2', content: 'test content 2')
+Task.create(status: 'test title 3', content: 'test content 3')
