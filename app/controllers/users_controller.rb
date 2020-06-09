1233aa0af711006b1ecb3a4245931c03da182b10
yuuki.horie <yuzu0145.h@gmail.com>
2020-06-09 14:06:05 +0000

    for heroku2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,6 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
-  def index
-  end
-
   def show
-    @user = User.find(params[:id])
   end
-
   def new
     @user = User.new
   end
